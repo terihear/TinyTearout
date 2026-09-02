475aeaf6556af65c5190356c1c6ce84b3b00b3ea
terihear <ter@zipcon.net>
2026-09-02 15:00:36 -0700

    See

--- src/project.v
+++ src/project.v
@@ -154,8 +154,8 @@ module tt_um_terihear_tinytearout (
       end else if (ena) begin
          if (result_valid && !out_active) begin
             // Capture result, emit LO byte immediately
-            out_shift    <= rounded_result;
-            out_data     <= rounded_result[7:0];
+            out_shift    <= multiplicand[22:7];
+            out_data     <= out_shift[7:0];
             out_word_sel <= 1'b0;
             out_active   <= 1'b1;
          end else if (out_active && !out_word_sel) begin
